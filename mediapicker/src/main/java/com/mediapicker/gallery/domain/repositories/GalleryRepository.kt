6e6f560f841e6664757b65d61501b640e937520e
Reinaldo Riant <reinaldo.perdana@admo.astra.co.id>
2025-03-24 10:16:23 +0700

    [AT-496] Gallery Service Optimization to avoid ANR in Media Picker

--- mediapicker/src/main/java/com/mediapicker/gallery/domain/repositories/GalleryRepository.kt
+++ mediapicker/src/main/java/com/mediapicker/gallery/domain/repositories/GalleryRepository.kt
@@ -4,5 +4,5 @@ import com.mediapicker.gallery.domain.entity.PhotoAlbum
 
 interface GalleryRepository {
 
-    fun getAlbums(): HashSet<PhotoAlbum>
+   fun getAlbums(): HashSet<PhotoAlbum>
 }
--- mediapicker/src/main/java/com/mediapicker/gallery/presentation/viewmodels/LoadAlbumViewModel.kt
+++ mediapicker/src/main/java/com/mediapicker/gallery/presentation/viewmodels/LoadAlbumViewModel.kt
@@ -2,18 +2,28 @@ package com.mediapicker.gallery.presentation.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mediapicker.gallery.domain.entity.PhotoAlbum
 import com.mediapicker.gallery.domain.repositories.GalleryRepository
-import java.util.*
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
 
 class LoadAlbumViewModel constructor(private val galleryRepository: GalleryRepository) : ViewModel(){
 
     private val albumLiveData = MutableLiveData<HashSet<PhotoAlbum>>()
+    private val singleThreadDispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
 
     fun getAlbums() = albumLiveData
 
-    fun loadAlbums(){
-        albumLiveData.postValue(galleryRepository.getAlbums())
+    fun loadAlbums() {
+        viewModelScope.launch(singleThreadDispatcher) {
+            albumLiveData.postValue(galleryRepository.getAlbums())
+        }
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        singleThreadDispatcher.close()
+    }
 }
